b6b045c815744bdb23b0695b41f0dea4e21d0d0f
Nao <nao@wedge>
2010-10-10 12:20:07 +0000

    + Changed Calendar icon to a new, sprited one. Added sprited icon for Register. Fixed icon widths. (Subs.php) * Updated to SMF rev 10150, with a lot of minor tweaks to layout and styling.   * Missing icon in profile/ignore boards. Markup/css to allow large content above the linktree (Profile.template.php, index.template.php, index.css) (NB: was already done in Wedge anyway...)   * Minor cleaning up in admin. Better highlighting of search results. Defined new_win class. Better description for post options (Admin.template.php, admin.css, index.css, Post.english.php)   * Fixed post-breaking bug in IE6 and IE7. Removed $context['right_to_left'] from two templates. (ie6.css, ie7.css, index.css, rtl.css, Display.template.php, Memberlist.template.php)   * Fixed board selection in profile and help. Cleaned up stats markup. (Profile.template.php, Help.template.php, Stats.template.php, index.css, rtl.css)

--- Post.english.php
+++ Post.english.php
@@ -121,7 +121,7 @@ $txt['attach_restrict_attachmentSizeLimit'] = 'maximum individual size %1$dKB';
 $txt['attach_restrict_attachmentNumPerPostLimit'] = '%1$d per post';
 $txt['attach_restrictions'] = 'Restrictions:';
 
-$txt['post_additionalopt'] = 'Additional Options...';
+$txt['post_additionalopt'] = 'Attachments and other options&#133;';
 $txt['sticky_after'] = 'Sticky this topic.';
 $txt['move_after2'] = 'Move this topic.';
 $txt['back_to_topic'] = 'Return to this topic.';
