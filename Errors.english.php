02dfa750c49b62608534f98196f3efa22fd3af5a
Arantor <sleeping@myperch.org>
2011-07-12 07:56:12 +0000

    ! Add a time gate to registration. First visit to the registration screen should force a limit of 10 seconds on filling the main form out, if you have any errors, that limit should be reduced to 5 seconds on returning the form. There is, presently, no limit on accepting the registration agreement (should there be one?) (Register.php, Errors.english.php)

--- Errors.english.php
+++ Errors.english.php
@@ -244,6 +244,7 @@ $txt['error_wrong_verification_answer'] = 'You did not answer the verification q
 $txt['error_need_verification_code'] = 'Please enter the verification code below to continue to the results.';
 $txt['error_bad_file'] = 'Sorry but the file specified could not be opened: %1$s';
 $txt['error_bad_line'] = 'The line you specified is invalid.';
+$txt['error_too_quickly'] = 'You completed the registration form too quickly, faster than would normally be possible.';
 
 $txt['smiley_not_found'] = 'Smiley not found.';
 $txt['smiley_has_no_code'] = 'No code for this smiley was given.';
