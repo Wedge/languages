3d4efacfc1a3c6a89354410077c72e49d50eed0f
Nao <nao@wedge>
2013-01-23 14:05:25 +0000

    * Doing my share to help with the packman removal... So, removed log_packages-related code. Also silly comments. (readme_install.html, readme_update.html, readme_upgrade.html, sphinx_config.php, smfinfo.php, upgrade.php, upgrade.sql, install.sql, Class-DBPackages.php, PackageGet.php, Subs-Package.php, extra.rtl.css, mana.css)
    + Added Home language files and fixed Homepage to match what you're seeing on Wedge.org right now. This mostly allows me to keep the file out of its 'ignore' changelist, which is a bit confusing to me. (Home.template.php, Home.english.php, Home.french.php)

---
+++ Home.english.php
@@ -0,0 +1,13 @@
+<?php
+// Version: 2.0; Home
+// This is a sample file.
+
+$txt['wedge_home_title'] = 'Welcome to the upcoming computer-aided community builder.';
+$txt['wedge_home_intro'] = '
+			Built in PHP 5 upon the SMF platform by some of its top devs (see the <a href="http://wedge.org/pub/faq/">FAQ</a> for more) for several years,
+			Wedge allows you to create and maintain <b>message boards</b> with a modern twist. Blogs, HTML5, CSS3, jQuery, object
+			programming, UTF8, CSS pre-parsing, improved security, drafts, easier theming and modding? Will do! We think that once it\'s
+			released, you\'ll agree that Wedge is the <strong>best free forum software</strong> available.
+			Read the full <a href="http://wedge.org/pub/feats/">feature list</a>. With dozens of new features, including the exclusive Aeva Media gallery system,
+			Wedge puts control back into your hands. When is it coming out? When it\'s ready.
+			<a href="http://wedge.org/blog/">Stay tuned</a> and subscribe to our blog\'s <a href="http://wedge.org/blog/?action=feed;sa=news">RSS feed</a> to make sure not to miss it!';
