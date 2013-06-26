95e20ae1c889a2239630e89463530c4cb617c6e2
Nao <nao@wedge>
2013-06-26 21:08:28 +0000

    * Refactored debug junk area at the bottom of all pages, for several reasons: I wanted it to be shorter (it's now on average 3 lines only), so that infinite scrolling works better for admins, and to be readable when queries were expanded, which wasn't the case until now, uh. (Subs-Template.php, Stats.language.php)
    ! Fixed potential wrong redirection when expanding/collapsing queries. (ViewQuery.php)
    
    ! Fixed plugin CSS caching. Thanks John! (Subs-Cache.php)
    
    * Added another bot to BadBehavior. Per Arantor's suggestion. (Security.php)

--- Stats.english.php
+++ Stats.english.php
@@ -49,19 +49,19 @@ $txt['totalTimeLogged_m_short'] = 'm';
 
 // Debug related stats - when $db_show_debug is true.
 $txt['debug_report'] = '
-	Templates (%1$d): <em>%2$s</em>.<br>
-	Blocks (%3$d): <em>%4$s</em>.<br>
-	Language files (%5$d): <em>%6$s</em>.<br>
-	Style sheets (%7$d): <em>%8$s</em>.<br>
-	Files included (%9$d): %10$sKB (<a href="#" onclick="%11$s">list</a><span class="hide"><em>%12$s</em></span>).<br>
-	Peak memory use: %13$dKB.<br>';
+	<strong>Templates</strong> (%1$d): <em>%2$s</em>.
+	<strong>Blocks</strong> (%3$d): <a href="#" onclick="%11$s">list</a><span class="hide"><em>%4$s</em></span>.
+	<strong>Language files</strong> (%5$d): <a href="#" onclick="%11$s">list</a><span class="hide"><em>%6$s</em></span>.
+	<strong>Style sheets</strong> (%7$d): <a href="#" onclick="%11$s">list</a><span class="hide"><em>%8$s</em></span>.
+	<strong>Files included</strong> (%9$d): %10$sKB - <a href="#" onclick="%11$s">list</a><span class="hide"><em>%12$s</em></span>.
+	<strong>Peak memory use</strong>: %13$dKB.';
 $txt['debug_cache_hits'] = '
-	Cache hits (%1$d): %2$ss for %3$s bytes (<a href="#" onclick="%4$s">list</a><span class="hide"><em>%5$s</em></span>).<br>';
+	<strong>Cache hits</strong> (%1$d): %2$ss for %3$s bytes - <a href="#" onclick="%4$s">list</a><span class="hide"><em>%5$s</em></span>.';
 $txt['debug_cache_seconds_bytes'] = '%1$s %2$ss - %3$s bytes';
-$txt['debug_queries_used'] = 'Queries used: %1$d.';
-$txt['debug_queries_used_and_warnings'] = 'Queries used: %1$d, %2$d warnings.';
+$txt['debug_queries_used'] = '<strong>Queries used</strong>: %1$d';
+$txt['debug_queries_used_and_warnings'] = '<strong>Queries used</strong>: %1$d, %2$d warnings';
 $txt['debug_query_in_line'] = 'in <em>%1$s</em> line <em>%2$s</em>, ';
 $txt['debug_query_which_took'] = 'which took %1$s seconds.';
 $txt['debug_query_which_took_at'] = 'which took %1$s seconds at %2$s into request.';
-$txt['debug_show_queries'] = '[Show Queries]';
-$txt['debug_hide_queries'] = '[Hide Queries]';
+$txt['debug_show_queries'] = '<strong>Expand Queries</strong>';
+$txt['debug_hide_queries'] = '<strong>Hide Queries</strong>';
--- Stats.french.php
+++ Stats.french.php
@@ -49,19 +49,19 @@ $txt['totalTimeLogged_m_short'] = 'm';
 
 // Debug related stats - when $db_show_debug is true.
 $txt['debug_report'] = '
-	Modèles (%1$d)&#8239;: <em>%2$s</em>.<br>
-	Blocs (%3$d)&#8239;: <em>%4$s</em>.<br>
-	Fichiers de langue (%5$d)&#8239;: <em>%6$s</em>.<br>
-	Feuilles de style (%7$d)&#8239;: <em>%8$s</em>.<br>
-	Fichiers inclus (%9$d)&#8239;: %10$sko (<a href="#" onclick="%11$s">voir</a><span class="hide"><em>%12$s</em></span>).<br>
-	Mémoire max. utilisée&#8239;: %13$dko.<br>';
+	<strong>Modèles</strong> (%1$d)&#8239;: <em>%2$s</em>.
+	<strong>Blocs</strong> (%3$d)&#8239;: <a href="#" onclick="%11$s">voir</a><span class="hide"><em>%4$s</em></span>.
+	<strong>Fichiers de langue</strong> (%5$d)&#8239;: <a href="#" onclick="%11$s">voir</a><span class="hide"><em>%6$s</em></span>.
+	<strong>Feuilles de style</strong> (%7$d)&#8239;: <a href="#" onclick="%11$s">voir</a><span class="hide"><em>%6$s</em></span>.
+	<strong>Fichiers inclus</strong> (%9$d)&#8239;: %10$sko - <a href="#" onclick="%11$s">voir</a><span class="hide"><em>%12$s</em></span>.
+	<strong>Mémoire max. utilisée</strong>&#8239;: %13$dko.';
 $txt['debug_cache_hits'] = '
-	Appels au cache (%1$d)&#8239;: %2$ss pour %3$s octets (<a href="#" onclick="%4$s">voir</a><span class="hide"><em>%5$s</em></span>).<br>';
+	<strong>Appels au cache</strong> (%1$d)&#8239;: %2$ss pour %3$s octets - <a href="#" onclick="%4$s">voir</a><span class="hide"><em>%5$s</em></span>.';
 $txt['debug_cache_seconds_bytes'] = '%1$s %2$ss - %3$s octets';
-$txt['debug_queries_used'] = 'Requêtes utilisées&#8239;: %1$d.';
-$txt['debug_queries_used_and_warnings'] = 'Requêtes utilisées&#8239;: %1$d, %2$d avertissements.';
+$txt['debug_queries_used'] = '<strong>Requêtes utilisées</strong>&#8239;: %1$d';
+$txt['debug_queries_used_and_warnings'] = '<strong>Requêtes utilisées</strong>&#8239;: %1$d, %2$d avertissements';
 $txt['debug_query_in_line'] = 'Appelée dans <em>%1$s</em>, à la ligne <em>%2$s</em>, ';
 $txt['debug_query_which_took'] = 'et exécutée en %1$s secondes.';
 $txt['debug_query_which_took_at'] = 'et exécutée en %1$s secondes, à %2$s secondes du lancement du script.';
-$txt['debug_show_queries'] = '[Montrer les Requêtes]';
-$txt['debug_hide_queries'] = '[Cacher les Requêtes]';
+$txt['debug_show_queries'] = '<strong>Développer la liste</strong>';
+$txt['debug_hide_queries'] = '<strong>Cacher la liste</strong>';
