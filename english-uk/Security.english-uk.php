a86db6b13dc7f7aba164b0b851304c14bc49baed
Farjo <frazer.oades@gmail.com>
2014-01-30 16:31:43 -0800

    Update Security.english-uk.php

--- english-uk/Security.english-uk.php
+++ english-uk/Security.english-uk.php
@@ -1,4 +1,4 @@
 <?php
 // Version: 0.1; Security
 
-$txt['behavior_header'] = 'We apologize for the inconvience, but were not able to complete your request; as part of protection against spreading bad things like spam and malware, the defenses of this site found potentially suspicious behaviour that matched that of known spammers and/or malware distributors. Fortunately the problem is normally quite easy to fix.<br><br>The information for this was supplied by <a href="http://www.bad-behavior.ioerror.us/">Bad Behaviour</a>.';
+$txt['behavior_header'] = 'We apologise for the inconvience, but we are unable to complete your request; as part of protection against spreading bad things like spam and malware, the defences of this site found potentially suspicious behaviour that matched that of known spammers and/or malware distributors. Fortunately the problem is normally quite easy to fix.<br><br>The information for this was supplied by <a href="http://www.bad-behavior.ioerror.us/">Bad Behaviour</a>.';
