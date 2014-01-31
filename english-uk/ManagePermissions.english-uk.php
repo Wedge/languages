a1118556466bd1af9a8dc400a9fc8410395ba190
Farjo <frazer.oades@gmail.com>
2014-01-30 16:14:59 -0800

    Update ManagePermissions.english-uk.php

--- english-uk/ManagePermissions.english-uk.php
+++ english-uk/ManagePermissions.english-uk.php
@@ -5,7 +5,7 @@ $txt['permissionhelp_view_stats'] = 'The forum statistics is a page summarising 
 
 $txt['permissionhelp_manage_attachments'] = 'This permission allows access to the attachment centre, where all forum attachments and avatars are listed and can be removed.';
 
-$txt['permissionhelp_manage_smileys'] = 'This allows access to the smiley centre. In the smiley center you can add, edit and remove smileys and smiley sets. If you\'ve enabled customized message icons you are also able to add and edit message icons with this permission.';
+$txt['permissionhelp_manage_smileys'] = 'This allows access to the smiley centre. In the smiley centre you can add, edit and remove smileys and smiley sets. If you\'ve enabled customised message icons you are also able to add and edit message icons with this permission.';
 
 $txt['permissionname_access_mod_center'] = 'Access the moderation centre';
 $txt['permissionhelp_access_mod_center'] = 'With this permission any members of this group can access the moderation centre from where they will have access to functionality to ease moderation. Note that this does not in itself grant any moderation privileges.';
