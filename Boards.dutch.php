1d063dc9dbe1213f6edf49d1fa861862d27c26d2
MyMug <7french@gmail.com>
2017-04-17 14:09:31 +0200

    Create Boards.dutch.php (#38)

---
+++ Boards.dutch.php
@@ -0,0 +1,35 @@
+<?php
+// Version: 2.0; Boards
+
+$txt['view_unread_category'] = 'Ongelezen berichten';
+
+// And now for the moderation 'boards'
+$txt['moderation_cat'] = 'Moderatie';
+$txt['reported_posts'] = 'Gerapporteerde berichten';
+$txt['reported_board_desc'] = '<strong>Gerapporteerd</strong> door {author}<br>in {link}<br>{time}';
+$txt['reported_open'] = array(
+	1 => '1 openstaande melding',
+	'n' => '%1$s openstaande meldingen',
+);
+$txt['reported_closed'] = array(
+	1 => '1 gesloten melding',
+	'n' => '%1$s gesloten meldingen',
+);
+$txt['group_requests'] = 'Groeps verzoek';
+$txt['group_request_desc'] = 'Aanmelden <strong>{groupname}</strong><br>by {author}<br>{time}';
+$txt['group_req_open'] = array(
+	1 => '1 openstaand verzoek',
+	'n' => '%1$s openstaande verzoeken',
+);
+$txt['group_req_closed'] = array(
+	1 => '1 gesloten verzoek',
+	'n' => '%1$s gesloten verzoeken',
+);
+$txt['logs_board'] = 'Logboeken';
+$txt['logs_moderation'] = 'Moderatielog';
+$txt['logs_infractions'] = 'Overtredingen log';
+$txt['error_log_count'] = array(
+	0 => 'Geen fouten',
+	1 => '1 fout',
+	'n' => '%1$s fouten',
+);
