c3042a40739ea845f9e293782e52ce1c2bc797ef
MyMug <7french@gmail.com>
2017-04-17 14:05:08 +0200

    Create ManageBoards.dutch.php (#46)

---
+++ ManageBoards.dutch.php
@@ -0,0 +1,120 @@
+<?php
+// Version: 2.0; ManageBoards
+
+$txt['boards_and_cats'] = 'Beheer boards en categorieen';
+$txt['order'] = 'Positie';
+$txt['full_name'] = 'Volledige naam';
+$txt['name_on_display'] = 'Dit is de naam die bij je berichten verschijnt.';
+$txt['boards_and_cats_desc'] = 'Bewerk je categorieen en boards hier. Meerdere moderators voer je als volgt in: <em>&quot;gebruikersnaam,gebruikersnaam&quot;</em> (de gebruikersnamen en niet de getoonde namen!)<br>Om een nieuw board aan te maken, klik je op de \'Voeg board toe\' knop. Om het nieuwe board een sub-board van een huidig board te maken, selecteer je \'Kind van...\' bij de \'Positie\' keuzelijst bij het aanmaken van een nieuw board.';
+$txt['parent_members_only'] = 'Niet-gegroepeerde leden';
+$txt['parent_guests_only'] = 'Gasten';
+$txt['catConfirm'] = 'Weet je zeker dat je deze categorie wilt verwijderen ?';
+$txt['boardConfirm'] = 'Weet je zeker dat je dit board wilt verwijderen?';
+
+$txt['catEdit'] = 'Bewerk categorie';
+$txt['collapse_enable'] = 'Inklapbaar';
+$txt['collapse_desc'] = 'Sta gebruikers toe om deze categorie in te klappen';
+$txt['catModify'] = '(bewerk)';
+
+$txt['mboards_order_after'] = 'Na ';
+$txt['mboards_order_inside'] = 'Tussen ';
+$txt['mboards_order_first'] = 'Als eerste';
+
+$txt['mboards_new_board'] = 'Board toevoegen';
+$txt['mboards_new_cat_name'] = 'Nieuwe categorie';
+$txt['mboards_add_cat_button'] = 'Voeg categorie toe';
+$txt['mboards_new_board_name'] = 'Nieuw board';
+
+$txt['mboards_name'] = 'Naam';
+$txt['mboards_modify'] = 'bewerk';
+$txt['mboards_permissions'] = 'permissies';
+// Don't use entities in the below string.
+$txt['mboards_permissions_confirm'] = 'Weet je zeker dat je voor dit board wilt omschakelen naar lokale permissies?';
+
+$txt['mboards_delete_cat'] = 'Verwijder categorie';
+$txt['mboards_delete_board'] = 'Verwijder board';
+
+$txt['mboards_delete_cat_contains'] = 'Het verwijderen van deze categorie zal ook de boards onder deze categorie verwijderen, inclusief alle topics, berichten en bijlagen binnen elk board';
+$txt['mboards_delete_option1'] = 'Verwijder categorie en alle boards daarbinnen.';
+$txt['mboards_delete_option2'] = 'Verwijder categorie en verplaatst alle boards die daarin zitten';
+$txt['mboards_delete_board_contains'] = 'Het verwijderen van dit board zal ook de sub-boards daaronder verplaatsen, inclusief alle topics, berichten en bijlages binnen elk van die boards';
+$txt['mboards_delete_board_option1'] = 'Verwijder board en verplaats de sub-boards daarvan naar categorie-niveau.';
+$txt['mboards_delete_board_option2'] = 'Verwijder board en verplaats de sub-boards daarvan naar';
+$txt['mboards_delete_what_do'] = 'Selecteer wat je wilt doen met deze boards';
+$txt['mboards_delete_confirm'] = 'Bevestig';
+$txt['mboards_delete_cancel'] = 'Annuleer';
+
+$txt['mboards_category'] = 'Categorie';
+$txt['mboards_description'] = 'Beschrijving';
+$txt['mboards_description_desc'] = 'Een korte beschrijving van je board.';
+$txt['mboards_groups'] = 'Toegestane ledengroepen';
+$txt['mboards_groups_desc'] = 'Ledengroepen toegestaan in dit board.<br><em>Let op: als een lid ook maar deel is van een van de geselecteerde ledengroepen, heeft hij toegang tot dit board.</em>';
+$txt['mboards_groups_regular_members'] = 'Deze groep bevat alle leden die niet een andere primaire ledengroep toegekend gekregen hebben.';
+$txt['mboards_groups_post_group'] = 'Deze groep is een bericht-gerelateerde groep.';
+
+$txt['mboards_never'] = 'Nooit';
+$txt['mboards_view_board'] = 'Wie kan dit board zien';
+$txt['mboards_enter_board'] = 'Wie kan dit board invoeren';
+$txt['mboards_groups_view_enter_same'] = 'Alle groepen die een bord kan zien kan het invoeren ook';
+$txt['mboards_groups_need_deny_perm'] = 'Ik wil een groep ontzeggen van dit board';
+$txt['mboards_groups_everyone'] = 'iedereen';
+$txt['mboards_groups_everyone_desc'] = 'Gebruik deze optie om alle groepen hier onder snel te selecteren.';
+$txt['mboards_offlimits_msg'] = 'Bericht aan degenen die dit bord kunnen zien, maar geen toegang hebben:';
+
+$txt['mboards_moderators'] = 'Moderators';
+$txt['mboards_moderators_desc'] = 'Leden met speciale rechten in dit board.';
+$txt['mboards_count_posts'] = 'Berichten tellen';
+$txt['mboards_count_posts_desc'] = 'Reacties en topics verhogen het aantal geplaatste berichten van dit lid.';
+$txt['mboards_unchanged'] = 'Onveranderd';
+$txt['mboards_skin'] = 'Board Skin';
+$txt['mboards_skin_desc'] = 'Hiermee kunt u het uiterlijk van je forum alleen binnen dit board wijzigen.';
+$txt['mboards_skin_mobile'] = 'Board Mobiel Skin';
+$txt['mboards_skin_mobile_desc'] = 'Hetzelfde, maar toen toegankelijk vanaf een mobiel apparaat.';
+$txt['mboards_skin_default'] = '(algemeen forum standaard)';
+$txt['mboards_override_skin'] = 'Skin Lid overschrijven.';
+$txt['mboards_override_skin_desc'] = 'Skin van dit bord gebruiken, zelfs als het lid er niet voor koos de standaardinstellingen te gebruiken.';
+$txt['mboards_language'] = 'Taal van dit Board';
+$txt['mboards_language_desc'] = 'Hiermee kunt u de standaardtaal van het forum (zoals de tekst op knoppen) instellen  alleen binnen dit board. Merk op dat dit slechts standaard is, als een gebruiker een taalvoorkeur heeft ingesteld, zal deze voorkeur deze instelling overschrijven.';
+
+$txt['mboards_type'] = 'Board type';
+$txt['mboards_type_desc'] = 'Dit verandert de manier waarop het board homepage wordt gepresenteerd. Bord toont een tabel lijst van onderwerpen, gerangschikt naar laatste antwoord. Blog toont een lijst met berichten gesorteerd op datum, ongeacht datums van commentaar.';
+
+$txt['mboards_redirect'] = 'Doorschakelen naar adres';
+$txt['mboards_redirect_desc'] = 'Schakel deze optie in om iemand die dit board aanklikt door te schakelen naar een andere site.';
+$txt['mboards_redirect_url'] = 'Adres om naar door te schakelen';
+$txt['mboards_redirect_url_desc'] = 'Bijvoorbeeld: &quot;http://wedge.org&quot;.';
+$txt['mboards_redirect_newtab'] = 'Open het nieuwe adres in een nieuw tabblad';
+$txt['mboards_redirect_reset'] = 'Reset aantal klikken';
+$txt['mboards_redirect_reset_desc'] = 'Als je dit selecteert zal het aantal klikken op nul gezet worden.';
+$txt['mboards_current_redirects'] = 'momenteel: %1$s';
+$txt['mboards_redirect_disabled'] = 'NB: het board moet leeg zijn om dit aan te kunnen zetten.';
+$txt['mboards_redirect_disabled_recycle'] = 'NB: je kunt het prullenbakboard niet tot een doorschakelend board maken.';
+
+$txt['mboards_order_before'] = 'Voor';
+$txt['mboards_order_child_of'] = 'Kind van';
+$txt['mboards_order_in_category'] = 'In categorie';
+$txt['mboards_current_position'] = 'Huidige positie';
+$txt['no_valid_parent'] = 'Board %1$s heeft geen geldige bovenliggend board. Gebruik de functie \'Zoek en repareer fouten\' om dit te verhelpen.';
+
+$txt['mboards_recycle_disabled_delete'] = 'Opmerking: je dient eerst een andere board als recycleboard in te stellen of recyclen uit te zetten voordat je dit board kunt verwijderen.';
+
+$txt['mboards_settings_desc'] = 'Bewerk alle board en categorie instellingen.';
+$txt['groups_manage_boards'] = 'Groepen die boards en categorieen mogen bewerken';
+$txt['recycle_enable'] = 'Recyclen van berichten aanzetten';
+$txt['recycle_board'] = 'Board voor gerecyclede berichten';
+$txt['recycle_board_unselected_notice'] = 'Je hebt het recyclen van topics ingeschakeld zonder te specificeren in welk board ze geplaatst moeten worden. Deze feature zal niet beschikbaar worden tot je een board gespecificeerd hebt.';
+$txt['countChildPosts'] = 'Tel berichten van sub-boards bij het totaal van de hoofdboards';
+$txt['ignorable_boards'] = 'Welke borden kunnen gebruikers negeren?';
+$txt['display_flags'] = 'Weergeven vlaggen op de boardindex?';
+$txt['flags_none'] = 'Geen vlaggen';
+$txt['flags_specified'] = 'Vlaggen voor boards die een taal ingesteld hebben';
+$txt['flags_all'] = 'Vlaggen voor alle boards';
+$txt['modcenter_category'] = 'Positie van de categorie Moderatie';
+$txt['modcenter_category_first'] = 'Als eerste catagorie';
+$txt['modcenter_category_after'] = 'Na de "%1$s" categorie';
+
+$txt['mboards_select_destination'] = 'Selecteer de gewenste positie voor het board \'<strong>%1$s</strong>\'';
+$txt['mboards_cancel_moving'] = 'Verplaatsen afbreken';
+$txt['mboards_move'] = 'verplaats';
+
+$txt['mboards_no_cats'] = 'Er zijn momenteel geen categorieen of boards geconfigureerd.';
