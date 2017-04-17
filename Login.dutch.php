102f45ddb1df1b72eda8b709f2dcf18c8456da9a
MyMug <7french@gmail.com>
2017-04-17 14:10:03 +0200

    Create Login.dutch.php (#44)

---
+++ Login.dutch.php
@@ -0,0 +1,149 @@
+<?php
+// Version: 2.0; Login
+
+$txt['maintain_mode'] = 'Onderhoudstand';
+$txt['admin_login'] = 'Inloggen Beheerder';
+$txt['username_no_exist'] = 'Gebruikersnaam bestaat niet.';
+$txt['no_user_with_email'] = 'Er zijn geen gebruikersnamen gekoppeld aan dit e-mail adres.';
+$txt['name_in_use'] = 'Gebruikersnaam is al in gebruik door een ander lid.';
+$txt['mins_logged_in'] = 'Minuten ingelogd blijven.';
+$txt['always_logged_in'] = 'Altijd ingelogd blijven.';
+
+// Registration agreement page.
+$txt['registration_agreement'] = 'Registratie overeenkomst';
+$txt['registration_reagreement_force'] = 'De voorwaarden van de overeenkomst zijn veranderd sinds de laatste keer,om blijvend gebruik te kunnen maken van deze site, dient u eerst akkoord te gaan met de nieuwe voorwaarden.';
+$txt['registration_reagreement_postonly'] = 'De voorwaarden van de overeenkomst zijn veranderd sinds de laatste keer.U kunt doorgaan met deze site door te bladeren, maar om berichten te kunnen plaatsen moet u akkoord gaan met de nieuwe voorwaarden.';
+$txt['agreement_agree'] = 'Ik accepteer de overeenkomst voorwaarden.';
+$txt['agreement_agree_coppa_above'] = 'Ik accepteer de overeenkomst voorwaarden.En ik ben tenminste %1$d jaar oud.';
+$txt['agreement_agree_coppa_below'] = 'k accepteer de overeenkomst voorwaarden.En ik ben jonger dan %1$d jaar.';
+$txt['noagreement_coppa_above'] = 'Ik ben tenminste %1$d jaar oud.';
+$txt['noagreement_coppa_below'] = 'Ik ben jonger dan %1$d jaar.';
+$txt['agreement_reagreed'] = 'Bedankt voor het accepteren van de overeenkomst voorwaarden.';
+$txt['agreement_return_to'] = 'Terug naar waar ik gebleven was';
+
+// Registration form.
+$txt['registration_form'] = 'Registratie formulier';
+$txt['need_username'] = 'Je moet een gebruikersnaam invullen';
+$txt['no_password'] = 'Je hebt geen wachtwoord ingevuld';
+$txt['incorrect_password'] = 'Wachtwoord niet correct';
+$txt['choose_username'] = 'Kies gebruikersnaam';
+$txt['choose_timezone'] = 'In welke tijdszone bevind u zich?';
+$txt['registration_successful'] = 'Registratie geslaagd';
+$txt['now_a_member'] = 'Gefeliciteerd! U bent nu lid van dit forum';
+$txt['email_placeholder'] = 'email@address.com';
+$txt['valid_email_needed'] = 'Vul een geldig e-mailadres in, %1$s';
+$txt['required_info'] = 'Verplichte velden';
+$txt['additional_information'] = 'Additionele informatie';
+$txt['warning'] = 'Info!!';
+$txt['only_members_can_access'] = 'Autorisatie Webmaster is vereist ';
+$txt['login_below'] = 'Na autorisatie inloggen op  %1$s.';
+$txt['login_below_register'] = 'Log hieronder in of <a href="%1$s">om je te registreren</a> met %2$s.';
+
+$txt['login_hash_error'] = 'Wachtwoordbeveiliging is recentelijk bijgewerkt. Vul je wachtwoord opnieuw in.';
+
+$txt['ban_register_prohibited'] = 'Sorry, je mag je niet registreren op dit forum';
+
+$txt['activate_account'] = 'Accountactivering';
+$txt['activate_success'] = 'Je accountactivering verliep succesvol. Je kunt nu inloggen.';
+$txt['activate_not_completed1'] = 'Je e-mailadres moet eerst gevalideerd worden voor je kunt inloggen.';
+$txt['activate_not_completed2'] = 'Nieuwe activeringsemail nodig?';
+$txt['activate_after_registration'] = 'Bedankt voor het registreren. Je ontvangt een e-mail met een link om je account te activeren.  Als je na verloop van tijd geen e-mail hebt ontvangen, check dan even je spamfolder.';
+$txt['invalid_userid'] = 'Gebruiker bestaat niet';
+$txt['invalid_activation_code'] = 'Ongeldige activeringscode';
+$txt['invalid_activation_username'] = 'Gebruikersnaam of e-mailadres';
+$txt['invalid_activation_new'] = 'Type als je met een foutief e-mailadres bent geregistreerd het juiste e-mailadres en je wachtwoord hier in.';
+$txt['invalid_activation_new_email'] = 'Nieuw e-mailadres';
+$txt['invalid_activation_password'] = 'Oude wachtwoord';
+$txt['invalid_activation_resend'] = 'Activeringscode opnieuw versturen.';
+$txt['invalid_activation_known'] = 'Als je de activeringscode al kent, kun je die hier intypen.';
+$txt['invalid_activation_retry'] = 'Activeringscode.';
+$txt['invalid_activation_submit'] = 'Activeren';
+
+$txt['coppa_no_concent'] = 'De forumbeheerder heeft nog geen toestemming van je ouders/voogden ontvangen voor je account.';
+$txt['coppa_need_more_details'] = 'Meer details nodig?';
+
+$txt['awaiting_delete_account'] = 'Je hebt je account gemarkeerd voor verwijdering!<br >Als je je account wilt herstellen, vink dan &quot;Herstel mijn account&quot; aan en log dan opnieuw in.';
+$txt['undelete_account'] = 'Herstel mijn account';
+
+// Use numeric entities in the below three strings.
+$txt['change_password'] = 'Nieuwe wachtwoordgegevens';
+$txt['change_password_login'] = 'Je inloggegevens voor';
+$txt['change_password_new'] = 'zijn veranderd en je wachtwoord is opnieuw ingesteld. Hieronder zijn je nieuwe inlogdetails.';
+
+$txt['in_maintain_mode'] = 'Het forum bevindt zich in de onderhoudsmodus.';
+
+$txt['register_agree'] = 'Lees en accepteer de overeenkomst voor je jezelf registreert.';
+$txt['register_passwords_differ_js'] = 'De twee wachtwoorden komen niet overeen!';
+
+$txt['approval_after_registration'] = 'Bedankt voor je registratie. De beheerder moet je registratie goedkeuren alvorens je kunt inloggen. Je ontvangt hierover zeer binnenkort een e-mail.';
+
+$txt['admin_settings_desc'] = 'Hier kun je diverse instellingen veranderen met betrekking tot de registratie van nieuwe leden.';
+
+$txt['setting_registration_method'] = 'Registratiemethode voor nieuwe leden.';
+$txt['setting_registration_disabled'] = 'Registratie uitgeschakeld';
+$txt['setting_registration_standard'] = 'Directe registratie';
+$txt['setting_registration_activate'] = 'Activatie via e-mail';
+$txt['setting_registration_approval'] = 'Goedkeuring door beheerder';
+$txt['setting_registration_both'] = 'Activatie via e-mail, daarna goedkeuring door beheerder';
+$txt['setting_notify_new_registration'] = 'Breng beheerders op de hoogte als een nieuw lid zich geregistreerd heeft';
+$txt['setting_send_welcomeEmail'] = 'Stuur een welkomst-e-mail naar nieuwe leden';
+$txt['purge_unactivated_days'] = 'Schrap niet geactiveerde accounts na hoeveel dagen?';
+$txt['purge_unactivated_days_subtext'] = 'Wanneer ingesteld op 0, wordt er niets geschrapt';
+$txt['purge_unactivated_days_postinput'] = 'dagen';
+
+$txt['setting_coppaAge'] = 'Leeftijd waar beneden de registratie restricties gelden';
+$txt['setting_coppaAge_desc'] = '(0 om uit te schakelen)';
+$txt['setting_coppaType'] = 'Te nemen actie als een gebruiker onder de minimumleeftijd zich registreert.';
+$txt['setting_coppaType_reject'] = 'Verwerp de registratie';
+$txt['setting_coppaType_approval'] = 'Vereis ouderlijke/voogd goedkeuring';
+$txt['setting_coppaPost'] = 'Postadres waar het goedkeuringsformulier naar toe gestuurd moet worden';
+$txt['setting_coppaPost_desc'] = 'Alleen van toepassing als de leeftijdsrestrictie ingesteld staat';
+$txt['setting_coppaFax'] = 'Faxnummer waar de goedkeuring naar gefaxt kan worden';
+$txt['setting_coppaPhone'] = 'Telefoonnummer voor ouders om contact op te nemen voor leeftijdsrestrictievragen';
+
+$txt['admin_register'] = 'Registratie van een nieuw lid';
+$txt['admin_register_desc'] = 'Vanaf hier kun je nieuwe leden registreren en hen, indien gewenst, de details mailen';
+$txt['admin_register_username'] = 'Nieuwe gebruikersnaam';
+$txt['admin_register_email'] = 'E-mail adres';
+$txt['admin_register_password'] = 'Wachtwoord';
+$txt['admin_register_username_desc'] = 'Gebruikersnaam van het nieuwe lid';
+$txt['admin_register_email_desc'] = 'E-mailadres van het lid';
+$txt['admin_register_password_desc'] = 'Wachtwoord voor nieuw lid';
+$txt['admin_register_email_detail'] = 'E-mail nieuw wachtwoord naar gebruiker';
+$txt['admin_register_email_detail_desc'] = 'E-mailadres vereist zelfs indien niet aangevinkt';
+$txt['admin_register_email_activate'] = 'Gebruiker dient zijn account te activeren';
+$txt['admin_register_group'] = 'Primaire ledengroep';
+$txt['admin_register_group_desc'] = 'Primaire ledengroep waar het nieuwe lid toe zal behoren';
+$txt['admin_register_group_none'] = '(geen primaire ledengroep)';
+$txt['admin_register_done'] = 'Het lid %1$s is met succes geregistreerd!';
+
+$txt['coppa_title'] = 'Leeftijdsbeperkt forum';
+$txt['coppa_after_registration'] = 'Bedankt voor je regstratie op {forum_name_html_safe}. <br ><br >Als lid onder de leeftijd van {MINIMUM_AGE} jaar is er een wettelijke verplichting
+om de goedkeuring van ouders of voogd te krijgen voordat je gebruik mag maken van je account. Print onderstaand formulier uit om activering van je account te regelen:';
+$txt['coppa_form_link_popup'] = 'Laad het formulier in een nieuw venster';
+$txt['coppa_form_link_download'] = 'Download formulier als een tekstbestand';
+$txt['coppa_send_to_one_option'] = 'Zorg er daarna voor dat je ouders of voogd het ingevulde formulier opsturen naar:';
+$txt['coppa_send_to_two_options'] = 'Zorg er daarna voor dat je ouders of voogd het ingevulde formulier opsturen naar:';
+$txt['coppa_send_by_post'] = 'Post, naar het volgende adres:';
+$txt['coppa_send_by_fax'] = 'Fax, naar het volgende nummer:';
+$txt['coppa_send_by_phone'] = 'Eventueel kun je hen ook laten bellen naar de forumbeheerder op {PHONE_NUMBER}.';
+
+$txt['coppa_form_title'] = 'Toestemmingsformulier voor registratie op {forum_name_html_safe}';
+$txt['coppa_form_address'] = 'Adres';
+$txt['coppa_form_date'] = 'Datum';
+$txt['coppa_form_body'] = 'Ik {PARENT_NAME},<br ><br >geef toestemming aan {CHILD_NAME} (naam kind) om volwaardig geregistreerd lid te worden op het forum: {forum_name_html_safe} ,met de gebruikersnaam: {USER_NAME}.<br ><br >Ik begrijp dat bepaalde persoonlijke informatie door {USER_NAME} ingevuld getoond kan worden aan andere gebruikers van het forum.<br ><br >Getekend:<br >{PARENT_NAME} (Ouder/Voogd).';
+
+$txt['visual_verification_sound_again'] = 'Speel opnieuw';
+$txt['visual_verification_sound_direct'] = 'Heb je problemen met het beluisteren? Probeer een directe link naar het geluid.';
+
+$txt['registration_username_available'] = 'Gebruikersnaam is beschikbaar';
+$txt['registration_username_unavailable'] = 'Gebruikersnaam is niet beschikbaar';
+$txt['registration_username_check'] = 'Controleer beschikbaarheid gebruikersnaam';
+$txt['registration_password_short'] = 'Wachtwoord is te kort';
+$txt['registration_password_reserved'] = 'Wachtwoord bevat gebruikersnaam of e-mail';
+$txt['registration_password_numbercase'] = 'Wachtwoord moet zowel hoofdletters en kleine letters en cijfers bevatten.';
+$txt['registration_password_no_match'] = 'Wachtwoorden komen niet overeen';
+$txt['registration_password_valid'] = 'Wachtwoord is geldig';
+
+$txt['registration_errors_occurred'] = 'De onderstaande fouten ontstonden bij je registratie. Verbeter ze voor je doorgaat:';
+$txt['error_invalid_characters_username'] = 'Ongeldige karakters gebruikt in Gebruikersnaam.';
