36d81c5d56ba50db15f1e66de70163400a4ba798
MyMug <7french@gmail.com>
2017-04-17 14:08:38 +0200

    Create ManageMembers.dutch.php (#50)

---
+++ ManageMembers.dutch.php
@@ -0,0 +1,166 @@
+<?php
+// Version: 2.0; ManageMembers
+
+$txt['groups'] = 'Groepen';
+$txt['members_list'] = 'Ledenlijst';
+$txt['viewing_groups'] = 'Ledengroepen bekijken';
+$txt['visible_to_all'] = '(reeds zichtbaar voor iedereen)';
+$txt['viewing_members'] = 'Leden bekijken %1$s to %2$s';
+$txt['of_total_members'] = 'of %1$s alle leden';
+$txt['view_all_members'] = 'Bekijk alle Leden';
+
+$txt['membergroups_title'] = 'Ledengroepen beheren';
+$txt['membergroups_description'] = 'Ledengroepen zijn groepen van leden die dezelfde permissies hebben. Sommige ledengroepen zijn gebaseerd op het aantal geplaatste berichten. je kunt iemand toewijzen aan een ledengroep door hun profiel te selecteren en het veranderen van hun accountinstellingen.';
+$txt['membergroups_modify'] = 'Aanpassen';
+$txt['membergroups'] = 'Ledengroep';
+
+$txt['membergroups_add_group'] = 'Groep toevoegen';
+$txt['membergroups_regular'] = 'Reguliere ledengroepen';
+$txt['membergroups_post'] = 'Bericht-gerelateerde ledengroepen';
+
+$txt['membergroups_group_name'] = 'Naam ledengroep';
+$txt['membergroups_new_board'] = 'Zichtbare boards';
+$txt['membergroups_board_see'] = 'Boards die de ledengroep kan zien';
+$txt['membergroups_board_enter'] = 'Toegang deze ledengroep.......';
+$txt['membergroups_new_board_post_groups'] = '<em>Let op: normaal gesproken hebben bericht-gerelateerde groepen geen toegang nodig omdat de groep waarin ze zitten hen toegang zal geven.</em>';
+$txt['membergroups_new_as_inherit'] = 'erven van';
+$txt['membergroups_new_as_type'] = 'aan de hand van type';
+$txt['membergroups_new_as_copy'] = 'gebaseerd op';
+$txt['membergroups_new_copy_none'] = '(geen)';
+$txt['membergroups_can_edit_later'] = 'Je kunt deze later bewerken.';
+$txt['membergroups_view_enter_same'] = 'Boards die de ledengroep kan zien.';
+$txt['membergroups_need_deny_perm'] = 'Ik moet deze groep ontzeggen van enkele boards';
+$txt['group_boards_never'] = 'Nooit';
+$txt['group_boards_everything'] = 'Alles';
+$txt['group_boards_everything_desc'] = 'Gebruik deze optie om snel alle boards hieronder te selecteren.';
+
+$txt['membergroups_edit_group'] = 'Voeg een ledengroep toe';
+$txt['membergroups_edit_name'] = 'Naam van de ledengroep';
+$txt['membergroups_edit_inherit_permissions'] = ' Permissies erven';
+$txt['membergroups_edit_inherit_permissions_desc'] = 'Selecteer &quot;Niet&quot; om unieke permissies voor deze groep in te stellen.';
+$txt['membergroups_edit_inherit_permissions_no'] = 'Niet: gebruik unieke permissies';
+$txt['membergroups_edit_inherit_permissions_from'] = 'Erven van';
+$txt['membergroups_edit_hidden'] = 'Zichtbaarheid';
+$txt['membergroups_edit_hidden_no'] = 'Zichtbaar';
+$txt['membergroups_edit_hidden_boardindex'] = 'Zichtbaar, met uitzondering van groepssleutel';
+$txt['membergroups_edit_hidden_all'] = 'Onzichtbaar';
+$txt['membergroups_edit_hidden_warning'] = 'Weet je zeker dat je niet toe wilt staan dat leden deze groep als primaire groep ingesteld krijgen?<br><br>Dit zal tot gevolg hebben dat de groep alleen als additionele groep toegekend zal kunnen worden, en dat leden die hem nu als primaire groep ingesteld hebben, hem als additionele groep krijgen.';
+$txt['membergroups_edit_desc'] = 'Groepsomschrijving';
+$txt['membergroups_edit_group_type'] = 'Groepsoort';
+$txt['membergroups_edit_select_group_type'] = 'Selecteer groepsoort';
+$txt['membergroups_group_type_private'] = 'Prive<span class="smalltext">(lidmaatschap moet toegekend worden)</span>';
+$txt['membergroups_group_type_protected'] = 'Beschermd <span class="smalltext">(Alleen beheerders kunnen beheren en toewijzen)</span>';
+$txt['membergroups_group_type_request'] = 'Aanvraagbaar <span class="smalltext">(leden kunnen lidmaatschap aanvragen)</span>';
+$txt['membergroups_group_type_free'] = 'Vrij <span class="smalltext">(leden kunnen toe- en aftreden naar believen)</span>';
+$txt['membergroups_group_type_post'] = 'Posts-gebaseerd <span class="smalltext">(lidmaatschap gebaseerd op aantal berichten)</span>';
+$txt['membergroups_min_posts'] = 'Benodigd aantal berichten';
+$txt['membergroups_online_color'] = 'Kleur in de profiel lijst';
+$txt['membergroups_online_color_desc'] = 'e.g. #ffcc99';
+$txt['membergroups_additional_formatting'] = 'Extra opmaak voor profiel koppelingen';
+$txt['membergroups_add_format_bold'] = 'Bold';
+$txt['membergroups_add_format_italic'] = 'italic';
+$txt['membergroups_add_format_underline'] = 'Onderstreept';
+$txt['membergroups_add_format_strike'] = 'doorstrepen';
+$txt['membergroups_add_format_free'] = 'Vrij formaat';
+$txt['membergroups_star_count'] = 'Aantal rang afbeeldingen';
+$txt['membergroups_star_count_note'] = 'Of stel in op 1 om een ​​badge te gebruiken in plaats van sterren, bijvoorbeeld.';
+$txt['membergroups_star_image'] = 'Rang afbeelding bestandsnaam';
+$txt['membergroups_star_image_note'] = 'Je kunt $language gebruiken voor de taal van de gebruiker.';
+$txt['membergroups_max_messages'] = 'Maximum aantal persoonlijke berichten';
+$txt['membergroups_max_messages_note'] = '0 = onbeperkt';
+$txt['membergroups_edit_save'] = 'Opslaan';
+$txt['membergroups_delete'] = 'Verwijder';
+$txt['membergroups_confirm_delete'] = 'Weet je zeker dat je deze ledengroep wilt verwijderen?!';
+
+$txt['membergroups_members_title'] = 'Groep bekijken';
+$txt['membergroups_members_group_members'] = 'Groepsleden';
+$txt['membergroups_members_no_members'] = 'Deze groep is momenteel leeg';
+$txt['membergroups_members_add_title'] = 'Voeg een lid toe aan deze groep';
+$txt['membergroups_members_add_desc'] = 'Lijst van leden om toe te voegen';
+$txt['membergroups_members_add'] = 'Voeg leden toe';
+$txt['membergroups_members_remove'] = 'Verwijder uit ledengroep';
+$txt['membergroups_members_last_active'] = 'Laatst aktief';
+$txt['membergroups_members_additional_only'] = 'Voeg alleen als additionele groep toe.';
+$txt['membergroups_members_group_moderators'] = 'Groepsbeheerders';
+$txt['membergroups_members_description'] = 'Omschrijving';
+$txt['membergroups_members_deadmin_confirm'] = 'Weet je zeker dat je jezelf van de beheerdersgroep wilt verwijderen?';
+
+$txt['membergroups_postgroups'] = 'Bericht-gerelateerde groepen';
+$txt['membergroups_settings'] = 'Ledengroepinstellingen';
+$txt['groups_manage_membergroups'] = 'Ledengroepen die ledengroepen mogen aanpassen.';
+$txt['group_text_show'] = 'Toon groep titels onder gebruikersnamen?';
+$txt['group_show_none'] = 'Niets';
+$txt['group_show_all'] = 'Alle groepen';
+$txt['group_show_normal'] = 'Gewone gebruikersgroepen';
+$txt['group_show_post'] = 'Bericht gerelateerde gebruikersgroepen';
+$txt['group_show_cond'] = 'Voorwaardelijke Post-based|Toon normale groepen. Als er geen groep is toegewezen, toont post-gebaseerde ledengroep in plaats daarvan.';
+
+$txt['membergroup_badges'] = 'ledengroep Badges';
+$txt['membergroup_badges_desc'] = 'Hier kan je kiezen welke ledengroepen badges zullen worden getoond en in welke volgorde ze worden aangegeven, door ze te slepen.';
+$txt['membergroup_badges_nobadge'] = '(geen badge)';
+
+$txt['membergroup_badge_postcount'] = '(aantal berichten groep)';
+$txt['membergroup_show_when_never'] = 'Toon nooit deze badge';
+$txt['membergroup_show_when_always'] = 'Deze badge altijd tonen';
+$txt['membergroup_show_when_primary'] = 'Alleen wanneer dit primaire badge is van de gebruiker';
+$txt['membergroup_show_when_cond'] = 'Tonen als er geen andere badges is';
+$txt['membergroup_show_when_desc_never'] = 'Zelfs als een gebruiker in deze groep zit, badge is niet tonen.';
+$txt['membergroup_show_when_desc_always'] = 'Als een gebruiker in deze groep zit, Toon deze badge.';
+$txt['membergroup_show_when_desc_primary'] = 'Toon alleen deze badge als deze groep de primaire groep is van de gebruiker.';
+$txt['membergroup_show_when_desc_cond'] = 'Toon alleen deze badge als de gebruiker in deze groep zit en ze hebben geen andere badges';
+$txt['show_group_key'] = 'Toon de groep legenda in het Info Centrum';
+$txt['ban_group'] = 'Voeg gebande gebruikers toe aan een groep';
+$txt['ban_group_subtext'] = 'Dit geldt alleen voor volledig gebande gebruikers, en alleen voor weergave doeleinden.';
+$txt['ban_group_none'] = 'Gebande gebruikers niet toevoegen aan een groep';
+
+$txt['membergroups_select_permission_type'] = 'Selecteer toestemming profiel';
+$txt['membergroups_select_visible_boards'] = 'Toon boards';
+$txt['membergroups_members_top'] = 'Leden';
+$txt['membergroups_name'] = 'Naam';
+$txt['membergroups_stars'] = 'Rang';
+
+$txt['admin_browse_approve'] = 'Leden waarvan hun accounts in afwachting zijn van goedkeuring';
+$txt['admin_browse_approve_desc'] = 'Vanaf hier kunt u alle leden die in afwachting zijn van hun accounts goedkeuring beheren.';
+$txt['admin_browse_activate'] = 'Leden waarvan hun accounts wachten op activering';
+$txt['admin_browse_activate_desc'] = 'Dit scherm geeft alle leden die nog steeds niet hun accounts geactiveerd hebben op uw forum.';
+
+$txt['admin_browse_username'] = 'Gebruikersnaam';
+$txt['admin_browse_email'] = 'E-mail adres';
+$txt['admin_browse_ip'] = 'IP Adres';
+$txt['admin_browse_registered'] = 'Geregistreerd';
+$txt['admin_browse_login'] = 'Laatst ingelogd';
+$txt['admin_browse_id'] = 'ID';
+$txt['admin_browse_with_selected'] = 'Geselecteerd met';
+$txt['admin_browse_no_members_approval'] = 'Er zijn geen leden die momenteel wachten op goedkeuring.';
+$txt['admin_browse_no_members_activate'] = 'Er zijn geen leden die momenteel nog niet hun account hebben geactiveerd.';
+
+$txt['admin_browse_warn'] = 'Alle geselecteerde leden?';
+$txt['admin_browse_outstanding_warn'] = 'Alle betrokken leden?';
+$txt['admin_browse_w_approve'] = 'Goedgekeurd';
+$txt['admin_browse_w_activate'] = 'Activeren';
+$txt['admin_browse_w_delete'] = 'Verwijder';
+$txt['admin_browse_w_reject'] = 'Verwerpen';
+$txt['admin_browse_w_remind'] = 'Herinneren';
+$txt['admin_browse_w_approve_deletion'] = 'Goedgekeurd (Verwijder Account)';
+$txt['admin_browse_w_email'] = 'en stuur e-mail';
+$txt['admin_browse_w_approve_require_activate'] = 'Goedkeuren en vereist activatie';
+
+$txt['admin_browse_filter_by'] = 'Filteren op';
+$txt['admin_browse_filter_show'] = 'Weergave';
+$txt['admin_browse_filter_type_0'] = 'Ongeactiveerde nieuwe accounts';
+$txt['admin_browse_filter_type_2'] = 'Ongeactiveerde gewijzigde E-mail ';
+$txt['admin_browse_filter_type_3'] = 'Niet goedgekeurde Nieuw Accounts';
+$txt['admin_browse_filter_type_4'] = 'Niet goedgekeurde Accounts verwijdering';
+$txt['admin_browse_filter_type_5'] = 'Niet goedgekeurde "onder leeftijd" Accounts';
+
+$txt['admin_browse_outstanding'] = 'Niet-afgehandelde leden';
+$txt['admin_browse_outstanding_days_1'] = 'Met alle leden die zich langer dan hebben geregistreerd';
+$txt['admin_browse_outstanding_days_2'] = 'Dagen geleden';
+$txt['admin_browse_outstanding_perform'] = 'Voer de volgende actie uit';
+$txt['admin_browse_outstanding_go'] = 'Actie uitvoeren';
+
+$txt['check_for_duplicate'] = 'Controleer op duplicaten';
+$txt['dont_check_for_duplicate'] = 'Controleer niet op duplicaten';
+$txt['duplicates'] = 'Duplicaten';
+
+$txt['not_activated'] = 'Niet geactiveerd';
